f4eb98625edb60ca4b09f0330ba100e17003fea3
Thomas Flanagan <thomas@tcflanagan.com>
2026-09-19 12:48:47 -0400

    Modify MySql integration tests so single-use fixtures go inside the test class.

--- test/TcfOss.DatabaseManager.MySql.IntegrationTests/DefinitionMapping/SimpleSchemaDifferTests_MySql_08_04.cs
+++ test/TcfOss.DatabaseManager.MySql.IntegrationTests/DefinitionMapping/SimpleSchemaDifferTests_MySql_08_04.cs
@@ -1,13 +1,27 @@
+using TcfOss.DatabaseManager.Core.Configuration.Attributes;
+using TcfOss.DatabaseManager.MySql.IntegrationTests.DatabaseFixtures;
 using Testcontainers.MySql;
+using Xunit.Sdk;
 
 namespace TcfOss.DatabaseManager.MySql.IntegrationTests.DefinitionMapping;
 
 // ReSharper disable once UnusedMember.Global
 public sealed class SimpleSchemaDifferTests_MySql_08_04
-    : SimpleSchemaDifferTests<MySqlBuilder, MySqlContainer, SimpleSchemaDifferFixture_MySql_08_04>
+    : SimpleSchemaDifferTests<MySqlBuilder, MySqlContainer, SimpleSchemaDifferTests_MySql_08_04.ThisFixture>
 {
-    public SimpleSchemaDifferTests_MySql_08_04(SimpleSchemaDifferFixture_MySql_08_04 fixture)
+    public SimpleSchemaDifferTests_MySql_08_04(ThisFixture fixture)
     {
         Fixture = fixture;
     }
+
+    public sealed class ThisFixture : SimpleSchemaDifferFixture<MySqlBuilder, MySqlContainer>
+    {
+        protected override SqlDialect Dialect => SqlDialect.MySql;
+
+        public ThisFixture(IMessageSink messageSink)
+        {
+            DbFixture = new MySqlFixture_08_04(messageSink);
+        }
+    }
+
 }
---
+++ test/TcfOss.DatabaseManager.MySql.IntegrationTests/DownloadSchema/DownloadSchemaTests_MySql_08_04.cs
@@ -0,0 +1,33 @@
+﻿using TcfOss.DatabaseManager.Core.Configuration.Attributes;
+using TcfOss.DatabaseManager.Core.IntegrationTests;
+using TcfOss.DatabaseManager.MySql.IntegrationTests.DatabaseFixtures;
+using Testcontainers.MySql;
+using Xunit.Sdk;
+
+namespace TcfOss.DatabaseManager.MySql.IntegrationTests.DownloadSchema;
+
+// ReSharper disable once UnusedMember.Global
+public class DownloadSchemaTests_MySql_08_04 : DownloadSchemaTests<MySqlBuilder, MySqlContainer, DownloadSchemaTests_MySql_08_04.ThisFixture>
+{
+    protected override string IfClauseOpen => "(";
+    protected override string IfClauseClose => ")";
+
+    public DownloadSchemaTests_MySql_08_04(ThisFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+
+    public class ThisFixture : DownloadSchemaFixture<MySqlBuilder, MySqlContainer>
+    {
+        protected override bool RemoveSlashesBeforeQuotesGenerationExpression => true;
+        protected override SqlDialect Dialect => SqlDialect.MySql;
+
+        public ThisFixture(IMessageSink messageSink)
+        {
+            DbFixture = new MySqlFixture_08_04(messageSink);
+            FsProjectFixture = new FsProjectFixture();
+        }
+    }
+
+}
